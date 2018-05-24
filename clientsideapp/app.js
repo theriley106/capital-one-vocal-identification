dad517b8accbcf695deba96742e302218c2ed8d4
adamschueller <aschueller@zoominternet.net>
2018-05-24 12:44:46 -0400

    Express uses html

--- clientsideapp/app.js
+++ clientsideapp/app.js
@@ -10,9 +10,8 @@ var index = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
