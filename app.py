ea46aa5ccf3e3a118649cb75e7f57f518f9a7c68
Theriley106 <christopherlambert106@gmail.com>
2018-05-24 13:32:52 -0400

    Added time elapsed and error messages

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect, Markup, jsonify, make_response, send_from_directory, session
 import vocal
+import time
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -10,14 +11,25 @@ def index():
 
 @app.route('/submitSpeech', methods=['POST'])
 def submitSpeech():
+	start = time.time()
 	language = "ES"
 	if request.form['text']:
+		text = request.form['text']
 		tempDict = {}
-		tempDict['original_text'] = request.form['text']
+		tempDict['success'] = True
+		tempDict['message'] = "Hello from the software engineering summit!"
+		tempDict['original_text'] = text
 		tempDict['new_text'] = vocal.translateText(text, language)
 		tempDict['language'] = language
-		return jsonify({"Text": text, "newText": newText})
+
+		end = time.time()
+		tempDict['time_elapsed'] = (end - start)
+		return jsonify(tempDict)
 	else:
+		tempDict['success'] = False
+		tempDict['message'] = "Request failed - Are you sure you added form data?"
+		end = time.time()
+		tempDict['time_elapsed'] = (end - start)
 		return jsonify({"status": "404"})
 
 if __name__ == '__main__':
