f8eca210bcd613b0e7aaceae0fa44a5d7e50201e
adamschueller <aschueller@zoominternet.net>
2018-05-25 10:02:33 -0400

    changed dropdown language

--- clientsideapp/public/js/customer.js
+++ clientsideapp/public/js/customer.js
@@ -39,26 +39,18 @@ function testSpeech() {
   var language = document.getElementById("langSelect").value;
 
   var speechRecognitionList = new SpeechGrammarList();
-  
+
   recognition.grammars = speechRecognitionList;
   recognition.lang = language;
   recognition.interimResults = true;
   recognition.maxAlternatives = 0;
-  
+
   recognition.start();
   isRecognizing = true;
   var speechResult = [];
   var increment = -1;
 
   recognition.onresult = function() {
-    // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-    // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-    // It has a getter so it can be accessed like an array
-    // The first [0] returns the SpeechRecognitionResult at position 0.
-    // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-    // These also have getters so they can be accessed like arrays.
-    // The second [0] returns the SpeechRecognitionAlternative at position 0.
-    // We then return the transcript property of the SpeechRecognitionAlternative object
     speechResult.push(event.results[0][0].transcript);
     increment = increment + 1;
     outputPara.textContent = transcript + ' ' + speechResult[increment];
@@ -66,37 +58,12 @@ function testSpeech() {
   }
 
   recognition.onspeechend = function() {
-
     stopRecognition();
-    // startBtn.textContent = 'Start new test';
-
-
-}
-
-    recognition.onspeechend = function () {
-        stopRecognition();
-        // startBtn.textContent = 'Start new test';
-
-    }
-
-    recognition.onerror = function (event) {
-        // startBtn.textContent = 'Start new test';
-        // outputPara.textContent = 'Call Timed Out:' + event.error;
-    }
-
-  recognition.onaudiostart = function(event) {
-      //Fired when the user agent has started to capture audio.
-      console.log('SpeechRecognition.onaudiostart');
-  }
-
-  recognition.onaudioend = function(event) {
-      //Fired when the user agent has finished capturing audio.
-      console.log('SpeechRecognition.onaudioend');
   }
 
   recognition.onend = function(event) {
-    console.log('sent')
-      var transcript = outputPara.textContent + '. ';
+    console.log(lang.options[lang.selectedIndex].value)
+      var transcript = outputPara.textContent + '. | ' + lang.options[lang.selectedIndex].value;
       $.ajax({
           url: 'http://104.236.71.248:8000/updates',
           type: 'POST',
@@ -105,7 +72,6 @@ function testSpeech() {
             lang: language
           },
       }).done(function(resp) {
-          console.log(resp);
           /*sentiment = resp.sentiment;
           sentimentVal = sentiment + sentimentVal;
           sentimentCount = sentimentCount + 1;
@@ -113,35 +79,12 @@ function testSpeech() {
           console.log(sentimentCount);
           avgSentiment = sentimentVal/sentimentCount;
           sentPara.textContent = "Sentiment: " + avgSentiment.toFixed(2);*/
+          console.log(resp);
+          updateText(resp.text)
       });
       if(recurse){
-      testSpeech();
-        }
-      console.log('SpeechRecognition.onend');
-  }
-
-  recognition.onnomatch = function(event) {
-      //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
-      console.log('SpeechRecognition.onnomatch');
-  }
-
-  recognition.onsoundstart = function(event) {
-      //Fired when any sound � recognisable speech or not � has been detected.
-      console.log('SpeechRecognition.onsoundstart');
-  }
-
-  recognition.onsoundend = function(event) {
-      //Fired when any sound � recognisable speech or not � has stopped being detected.
-      console.log('SpeechRecognition.onsoundend');
-  }
-
-  recognition.onspeechstart = function (event) {
-      //Fired when sound that is recognised by the speech recognition service as speech has been detected.
-      console.log('SpeechRecognition.onspeechstart');
-  }
-  recognition.onstart = function(event) {
-      //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
-      console.log('SpeechRecognition.onstart');
+        testSpeech();
+      }
   }
 }
 
@@ -196,12 +139,9 @@ $(startBtn).click(function() {
         $('#microphone').find('i').addClass('fa-microphone');
         $('#microphone').find('i').removeClass('fa-microphone-slash');
         reset = true;
-<<<<<<< HEAD
+
         callTimer.textContent = "00:00:00";
-        callStatus.innerHTML = "Start speaking";
-=======
         callStatus.innerHTML = "Start Speaking";
->>>>>>> cda32daef8cefa966b29f6163c661f94209905fe
         startBtn.css("background-color", "green");
     }
 
