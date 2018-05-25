5a58e932f7975faf8fce761f462f905557f8e84a
Theriley106 <christopherlambert106@gmail.com>
2018-05-24 22:57:28 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ def submitSpeech():
 	tempDict['success'] = True
 	tempDict['message'] = "Hello from the software engineering summit!"
 	tempDict['original_text'] = text
-	tempDict['original_text_speech'] = vocal.generateURL(tempDict['new_text'], language)
 	tempDict['new_text'] = vocal.translateText(text, language)
+	tempDict['new_text_speech'] = vocal.generateURL(tempDict['new_text'], language)
 	tempDict['sentiment'] = float("{0:.2f}".format(analytics.getSentiment(text)))
 	tempDict['keywords'] = analytics.getKeywords(tempDict['new_text'])
 	tempDict['verbosity'] = float("{0:.2f}".format(float(len(' '.join(tempDict['keywords']).split(' '))) / float(len(text.split(" ")))))
